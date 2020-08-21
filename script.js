64ed037ad10d6d90de7d0ce39aa1a1c43038e920
LordCracken <vladislav.yakimovskiy@mail.ru>
2020-08-21 19:05:22 +0300

    refact: refact code with ES6 (arrays cycles, objects & other)

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-'use strict';
-const employers = ['АртеМ', 'максим', 'Владимир', 'сергей', 'НикиТа', 'евГений', ' Дарья', ' ', 'виктория ', 'ЕкаТерина', '', ' Андрей ', 'КИРИЛЛ'];
+const employers = ['АртеМ', 'максим', 'Владимир', 'сергей', 'НикиТа', 'евГений',
+  ' Дарья', ' ', 'виктория ', 'ЕкаТерина', '', ' Андрей ', 'КИРИЛЛ'
+];
 const nameCourse = 'Базовый React';
-const command = [];
-for (const employer of employers) if (employer.length > 0 && employer.trim() !== '') command.push(employer);
-for (let member of command) {
-  member = member.toLowerCase().trim();
-  member = member[0].toUpperCase() + member.slice(1);
-}
+const command = employers.filter(name => name.trim().length)
+  .map(name => name.trim().charAt(0).toUpperCase() + name.trim().substr(1).toLowerCase());
 
 const data = {
   cash: [3, 8, 3],
@@ -14,26 +11,20 @@ const data = {
   add: ['styled-components', 'firebase']
 };
 
-const calcCash = (own, ...args) => {
-  own = own || 0;
-  const everyCash = args[0];
-  let total = own;
-  for (let i = 0; i < everyCash.length; i++) {
-    total += +everyCash[i];
-  }
-  return total;
+const { cash, react, react: [first], add } = data;
+
+const calcCash = everyCash => everyCash.reduce((prev, curr) => prev + curr);
+
+const lesson = calcCash(cash);
+
+const makeBusiness = (director, allModule, gang, course, teacher = `Максим`) => {
+  const sumTech = [...react, ...add, 'и другие'];
+  console.log(
+    `Стартуем новый курс: ${course}. Владелец: ${director}, преподаватель: ${teacher}. Всего уроков: ${allModule}
+Команда Академии: ${gang}
+Первое что изучим будет ${first}. Он очень похож на HTML!
+Технологии которые мы изучим:
+${sumTech}`);
 };
 
-const lesson = calcCash(null, data.cash);
-
-const makeBusiness = (director, teacher, allModule, gang, course) => {
-  teacher = teacher || 'Максим';
-  const sumTech = data.react.concat(data.add, 'и другие');
-  console.log(`Стартуем новый курс: "${course}". Владелец: ${director}, преподаватель: ${teacher}. Всего уроков: ${allModule}. Команда Академии: ${gang}`);
-  console.log(`Первое что изучим будет ${data.react[0]}. Он очень похож на HTML!`);
-  console.log('Технологии которые мы изучим: ');
-  console.log.apply(null, sumTech);
-
-};
-
-makeBusiness.apply(null, ['Артем', null, lesson, command, nameCourse]);
+makeBusiness('Артем', lesson, command, nameCourse);
