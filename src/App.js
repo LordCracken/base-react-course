3f3cc58292623cfee54b135ab7c04ee24785be88
LordCracken <vladislav.yakimovskiy@mail.ru>
2020-08-22 03:20:35 +0300

    refact: refact layout in React

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Header from './Header';
+import Main from './Main';
+import Features from './Features';
+import Contact from './Contact';
+import Footer from './Footer';
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Features />
+      <Contact />
+      <Footer />
+    </>
+  );
+};
+
+export default App;
