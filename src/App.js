9bed5a066f1b0e0d2d7704be303c1bde70136ed4
LordCracken <vladislav.yakimovskiy@mail.ru>
2020-08-24 21:25:26 +0300

    fix: replace projects files & delete old files

--- src/App.js
+++ src/App.js
@@ -1,16 +1,57 @@
 import React from 'react';
-import { Header, Main, Features, Contact, Footer } from './components';
+import { createGlobalStyle } from 'styled-components';
+import { NavBar } from './Components/NavBar';
 
-const App = () => {
-  return (
-    <>
-      <Header />
-      <Main />
-      <Features />
-      <Contact />
-      <Footer />
-    </>
-  );
-};
+const GlobalStyle = createGlobalStyle`
+  html {
+    box-sizing: border-box;
+  }
+
+  *, *::before, *::after {
+    box-sizing: inherit;
+  }
+
+  body {
+    margin: 0;
+    background-color: #f0f0f0;
+    font-family: Roboto, sans-serif;
+    font-size: 20px;
+    color: #000000;
+  }
+
+  img {
+    max-width: 100%;
+    height: auto;
+  }
+
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+
+  ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+  }
+
+  h1, h2, h3 {
+    margin: 0;
+    padding: 0;
+    font-family: Pacifico, cursive;
+  }
+
+  p {
+    margin: 0;
+    padding: 0;
+  }
+`;
+
+const App = () => (
+  <>
+    <GlobalStyle />
+    <NavBar />
+  </>
+);
 
 export default App;
